21f7467f78b7ede3bcfd928882a8763bc7c89991
aupowered <aupowered@gmail.com>
2025-10-09 15:00:17 -0600

    Create mighty-custom-js.js

---
+++ mighty-custom-js.js
@@ -0,0 +1,23 @@
+/**
+ * Adds table header text as data attributes to table cells, which will appear on mobile for responsive tables.
+ */
+function labelResponsiveTableCells() {
+	const tableCells = document.querySelectorAll('.rise-table-wrap--responsive td');
+	tableCells.forEach(tableCell => {
+		let cellIndex = tableCell.cellIndex;
+		let table = tableCell.closest('table');
+		let headerText = (table.querySelector('tr').cells[cellIndex].textContent);
+		tableCell.setAttribute('data-label', headerText);
+	});
+}
+function init() {
+	console.log('Mighty custom code running');
+        const observer = new MutationObserver(() => {
+                labelResponsiveTableCells();
+        });
+	// Observe lessons being loaded
+        observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+        });
+}
