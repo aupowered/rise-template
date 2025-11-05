576a00750662a66e50f22596f18cf5d806746ebc
aupowered <aupowered@gmail.com>
2025-11-05 14:18:54 -0700

    Add code to install Google Tag Manager
    Added new function to install Google Tag Manager.

--- mighty-custom-js.js
+++ mighty-custom-js.js
@@ -10,11 +10,29 @@ function labelResponsiveTableCells() {
 		tableCell.setAttribute('data-label', headerText);
 	});
 }
+/**
+ * Install Google Tag Manager.
+ */
+function installGTM() {
+	const gtmScriptTag = document.createElement('script');
+	const gtmNoScriptTag = document.createElement('noscript');
+	gtmScriptTag.text = `
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6ZG695F');
+`;
+	gtmNoScriptTag.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6ZG695F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+	document.head.prepend(gtmScriptTag);
+	document.body.prepend(gtmNoScriptTag);
+}
 function init() {
 	console.log('Mighty custom code running');
-        const observer = new MutationObserver(() => {
-                labelResponsiveTableCells();
-        });
+	installGTM();
+	const observer = new MutationObserver(() => {
+		labelResponsiveTableCells();
+	});
 	// Observe lessons being loaded
         observer.observe(document.body, {
                 childList: true,
